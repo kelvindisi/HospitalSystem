e9221e73a9e6c11845dc45e87102ef9b1de0bc1c
unknown <kelvindisi@yahoo.com>
2020-09-09 14:28:02 +0300

    added authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->integer('id_number')->unique();
+            $table->boolean('account_status')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
